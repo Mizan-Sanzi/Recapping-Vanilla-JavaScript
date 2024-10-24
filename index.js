b9b20037ba052476144baa2f0ec560aa777d636e
mizansanzi <pleasemailmizan@gmail.com>
2024-10-25 01:41:17 +0600

    index file added

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log('Hello'); //output: Hello
+var name = 'JavaScript';
+var number = 13;
+console.log('Hello2', name, number); //output: Hello2 JavaScript 13
+
+//alert('Hello3'); //ReferenceError 
+
+
