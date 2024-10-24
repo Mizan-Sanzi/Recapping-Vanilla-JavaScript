64404e04a8dea0024fc23262d6ed3c8a371fda7c
mizansanzi <pleasemailmizan@gmail.com>
2024-10-25 02:09:04 +0600

    code added

--- index.js
+++ index.js
@@ -5,4 +5,25 @@ console.log('Hello2', name, number); //output: Hello2 JavaScript 13
 
 //alert('Hello3'); //ReferenceError 
 
+var orangePrice = 200;
+var applePrice = 400;
+
+console.log(orangePrice+ applePrice);
+
+//number in javaScript
+
+var chairPrice = 1200;
+var tablePrice = '1400';
+
+console.log(chairPrice + parseInt(tablePrice));
+
+var first = 0.1;
+var second = 0.2;
+var total = first + second;
+
+console.log(total.toFixed(3));
+
+console.log(typeof total); //output: number
+
+
 
