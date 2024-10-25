17f1bbdf13954402a6867e4a1f424042b53365af
mizansanzi <pleasemailmizan@gmail.com>
2024-10-26 03:19:50 +0600

    code added

--- array.js
+++ array.js
@@ -9,11 +9,11 @@ console.log(typeof numbers); // output : object
 
 const newNumber = [];
 
-console.log(typeof newNumber); // output: object though it is an empty object
+console.log(typeof newNumber); // output: object -though it is an empty object
 
 const friends = ['Babul', 'Sabul', 'Kabul', 'Hablu']; // values of array can be string, boolean etc 
 
-console.log(friends.length); // length of array
+console.log(friends.length); // length of array, output: 4
 
 // index of array
 
@@ -21,6 +21,26 @@ const players = ['Sakib', 'Rafiq', 'Mushfiq', 'Abul', 'New'];
 
 players[0] = 'Mahmudullah'; // set value
 
-console.log(players[0]); // get value
-console.log(players[4]);
+console.log(players[0]); // get value - output: Mahmudullah
+console.log(players[4]); // output : New
 
+//array.includes()
+
+if (friends.includes('Babul')){
+    console.log('I am coming soon'); // this console will be printed
+}
+else{
+    console.log('First invite Babul then I will come');
+}
+
+
+if (friends.includes('Dabul')){
+    console.log('I am coming soon');
+}
+else{
+    console.log('First invite Babul then I will come'); //this console will be printed
+}
+
+//isArray
+
+console.log(Array.isArray(friends)); // true
